ae35ffd58528e7c87c39d0f4bb907e35c9eec87a
Mark Logan <mark@mystenlabs.com>
2023-06-15 16:27:59 -0700

    wip

--- crates/sui-core/src/test_utils.rs
+++ crates/sui-core/src/test_utils.rs
@@ -411,7 +411,7 @@ pub fn make_dummy_tx(
         Intent::sui_transaction(),
         vec![sender_sec],
     )
-    .verify_with_params(&Default::default())
+    .verify(&Default::default())
     .unwrap()
 }
 
